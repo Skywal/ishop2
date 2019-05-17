08c2b688d8287fc7453752d3a3cd45f956d1319d
Andrew Bloom <skaywoker6@gmail.com>
2019-05-17 07:27:11 +0300

    Create service functions file in 'libs' directory, make function to print arrays

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once dirname(__DIR__) . '/config/init.php';
+require_once LIBS . '/functions.php';
 
 new \ishop\App();
-var_dump(\ishop\App::$app->getProperties());
+debug(\ishop\App::$app->getProperties());
---
+++ vendor/ishop/core/libs/functions.php
@@ -0,0 +1,6 @@
+<?php
+//службові функції
+
+function debug ($arr){
+    echo '<pre>' . print_r($arr, true) . '</pre>';
+}
