752c5ef82135a04e5af17de8a36413c7a7f7a314
Andrew Bloom <skaywoker6@gmail.com>
2019-05-24 20:46:18 +0300

    Create base Controller abstract class and base class

---
+++ app/controllers/AppController.php
@@ -0,0 +1,12 @@
+<?php
+/// Контролер з спільними для всіх інших контролерів функціями
+
+namespace app\controllers;
+
+
+use ishop\base\Controller;
+
+class AppController extends Controller
+{
+
+}
--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,10 +3,16 @@
 
 namespace app\controllers;
 
+use ishop\base\Controller;
 
-class MainController
+class MainController extends AppController
 {
+
+
 	public function indexAction(){
+		debug($this->route);
+		debug($this->controller);
+
 		echo __FUNCTION__;
 	}
 }
